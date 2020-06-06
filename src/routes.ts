5d80985a72a28772a9e73728061a8548b2f69308
joaovictor3g <jvdias79797@gmail.com>
2020-06-06 15:38:18 -0300

    fix: last updates

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import multer from 'multer';
 import multerConfig from './config/multer';
+import { Joi, celebrate, Segments } from 'celebrate';
 
 const routes = Router();
 const upload = multer(multerConfig);
@@ -10,7 +11,24 @@ import pointController from './controllers/points/pointControllers';
 
 routes.get('/items', itemController.index);
 
-routes.post('/points', upload.single('image'), pointController.create);
+routes.post('/points', 
+    upload.single('image'), 
+    celebrate({
+        body: Joi.object().keys({
+            name: Joi.string().required(),
+            email: Joi.string().required(),
+            whatsapp: Joi.string().required(),
+            latitude: Joi.number().required(),
+            longitude: Joi.number().required(),
+            city: Joi.string().required(),
+            uf: Joi.string().required().max(2),
+            items: Joi.string().required(),
+        }),
+    }, {
+        abortEarly: false
+    }), pointController.create
+);
+
 routes.get('/points', pointController.index);
 routes.get('/points/:id', pointController.show);
 
--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,17 @@ import express from 'express';
 import path from 'path';
 import routes from './routes';
 import cors from 'cors';
+import { errors }  from 'celebrate';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
 app.use(routes);
 
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'tmp')))
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
+app.use(errors());
 
 app.listen(3333);
